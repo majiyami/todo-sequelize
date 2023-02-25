6245b95ba6e36ebe78c4846ab8481175ba6217e8
majiyami <andrew46100305@gmail.com>
2023-02-25 17:03:01 +0800

    todo-sequelize done

--- app.js
+++ app.js
@@ -1,18 +1,44 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
-const bcrypt = require('bcryptjs')
-const app = express()
-const PORT = 3000
+const session = require('express-session')
+const flash = require('connect-flash')
+const routes = require('./routes')
 
-app.engine('hbs', exphbs({defaultLaout:'main', extname: '.hbs'}))
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const PORT = process.env.PORT
+
+const usePassport = require('./config/passport')
+
+const app = express()
+
+app.engine('hbs', exphbs({ defaultLaout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs');
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 
-app.get('/', (req, res) => {
-  res.send('hello world')
+usePassport(app)
+
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
+  next()
 })
+
+app.use(routes)
+
+
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
